8cc13c408a668bead976b948059146e4119497a9
Artem <nwlf.420@gmail.com>
2021-11-02 22:14:51 +0300

    Initial Commit

---
+++ BGSoftTZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BGSoftTZ
+//
+//  Created by pro2017 on 02/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
